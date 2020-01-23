575f96685050b6e1a569571b14f3218a3dbb0957
Aashrey <aashrey9.jain@gmail.com>
2020-01-23 16:14:39 +0530

    app uses cleartext traffic

--- app/src/main/java/com/example/homecontroller/show_devices.kt
+++ app/src/main/java/com/example/homecontroller/show_devices.kt
@@ -3,6 +3,7 @@ package com.example.homecontroller
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
